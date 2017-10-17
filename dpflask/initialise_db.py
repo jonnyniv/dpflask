aeebe038f567ee010d708ef9a9041fa4f739c48f
Jonathan Niven <jonathan.niven@arm.com>
2017-10-17 16:45:50 +0100

    Fix init file

--- dpflask/initialise_db.py
+++ dpflask/initialise_db.py
@@ -1,4 +1,4 @@
-from dpflask import db
+import db
 import pandas as pd
 
 
@@ -7,7 +7,6 @@ def create_db():
     dataset = pd.read_json('https://data.iowa.gov/resource/wxib-fsgn.json?$limit=50000&$order=:id')
     dataset = dataset.drop('primary_county_coordinates', 1)
     dataset['month'] = dataset['date'].dt.month
-    print(dataset.head())
     dataset.to_sql('dpflask', connection, if_exists='replace')
     connection.commit()
     connection.close()
